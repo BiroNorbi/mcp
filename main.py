b01f46aee0f0616e22eccf55e80c24190b793401
Biró Norbert <biro.norbert@codespring.ro>
2025-10-30 13:09:15 +0200

    Basic MCP server created

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from mcp!")
+from fastmcp import FastMCP
 
+mcp = FastMCP("My MCP Application")
+
+@mcp.tool
+def greeting(name: str) -> str:
+    return f"Hello, {name}!"
 
 if __name__ == "__main__":
-    main()
+   mcp.run()
